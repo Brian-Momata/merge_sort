06d43f726281ccaeded719b8790e9d14ed68d07d
Brian-Momata <brianmomata@gmail.com>
2022-10-09 12:27:35 +0300

    Add merge method

--- merge_sort.rb
+++ merge_sort.rb
@@ -11,3 +11,29 @@ def merge_sort array
   end
 end
 
+def merge(left, right)
+  merged_array = []
+  l = 0
+  r = 0
+  while l < left.size && r < right.size
+    if left.empty?
+      merged_array.concat(right)
+    elsif right.empty?
+      merged_array.concat(left)
+    elsif left[l] < right[r]
+      merged_array << left[l]
+      l += 1
+    else
+      merged_array << right[r]
+      r += 1
+    end
+  end
+
+  if l == left.size # Passed through all elements in the left array
+    merged_array.concat(right[r..(right.size)])
+  elsif r == right.size
+    merged_array.concat(left[l..(left.size)])
+  end
+  merged_array
+end
+p merge_sort([4, 1, 5, 8, 2, 3])
